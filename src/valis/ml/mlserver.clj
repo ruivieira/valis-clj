7dd77865fd99d4baf93a0d525fe575704cba3781
Rui Vieira <rcardoso@redhat.com>
2023-07-03 21:39:40 +0100

    Initial commit

---
+++ src/valis/ml/mlserver.clj
@@ -0,0 +1,17 @@
+(ns valis.ml.mlserver
+   (:require [cheshire.core :as json]
+            [clojure.java.io :as io]))
+
+(defn write-model-settings
+  "Writes the model settings to the given directory."
+  [data dir]
+  (let [file (io/file dir "model-settings.json")]
+    (with-open [wtr (io/writer file)]
+      (json/generate-stream data wtr))))
+
+(defn read-model-settings
+  "Reads the model settings from the given directory and writes them to a file."
+  [dir]
+  (let [file (io/file dir "model-settings.json")]
+    (with-open [rdr (io/reader file)]
+      (json/parse-stream rdr true))))
---
+++ test/valis/core_test.clj
@@ -0,0 +1,7 @@
+(ns valis-clj.core-test
+  (:require [clojure.test :refer :all]
+            [valis-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
